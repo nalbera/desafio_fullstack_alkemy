452ee06fe648713b5ff8d786d8ae2168838752c4
Nelson Albera <nalbera@hotmail.com>
2023-05-27 22:31:38 -0300

    update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const testConnection = require('./src/database/service/testConnection');
 const PORT = process.env.PORT || 3001;
 
 testConnection().then(() => {
-    conn.sync({alter: false}).then(() => {
+    conn.sync({alter: true}).then(() => {
         server.listen(PORT, () => {
             console.log(`Server listening at ${PORT}`);
         });
